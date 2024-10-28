7852845d3e79c5054a63bbe429109579bb573688
Daniel Back <daniel.back9@outlook.com>
2024-10-28 21:28:02 +0100

    First commit
    Answer card transition dropdown effect missing

---
+++ faq/README.md
@@ -0,0 +1,1 @@
+# Collapsible-FAQ-cards
---
+++ faq/script/faqscrpt.js
@@ -0,0 +1,6 @@
+let faqQuestion = document.getElementById("faq-question__wrapper");
+
+faqQuestion.addEventListener("click", (e) => {
+    alert("hello world");
+})
+
